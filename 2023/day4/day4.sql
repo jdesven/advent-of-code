2f8e68128c5f44829719d3c6e2d7c1fdf62cba4b
jdesven <89591153+jdesven@users.noreply.github.com>
2025-03-25 22:34:52 +0100

    add 2023 day 4 part two

--- 2023/day4/day4.sql
+++ 2023/day4/day4.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS #raw_input
+-- IMPORT DATA
+
+DROP TABLE IF EXISTS #raw_input;
 
 CREATE TABLE #raw_input (
     input VARCHAR(MAX)
@@ -7,13 +9,11 @@ CREATE TABLE #raw_input (
 BULK INSERT #raw_input
 FROM '<PATH>'
 
--- PART ONE
-
 DROP TABLE IF EXISTS #winning_numbers;
 
 WITH winning_numbers_list AS (
     SELECT CAST(SUBSTRING(input, CHARINDEX(' ', input), 4) AS INT) AS card
-        , SUBSTRING(input, CHARINDEX(':', input) + 1, CHARINDEX('|', input) - CHARINDEX(':', input) - 1) as numbers
+        , SUBSTRING(input, CHARINDEX(':', input) + 1, CHARINDEX('|', input) - CHARINDEX(':', input) - 1) AS numbers
     FROM #raw_input
 )
 SELECT card
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS #owned_numbers;
 
 WITH owned_numbers_list AS (
     SELECT CAST(SUBSTRING(input, CHARINDEX(' ', input), 4) AS INT) AS card
-        , SUBSTRING(input, CHARINDEX('|', input) + 1, LEN(input)) as numbers
+        , SUBSTRING(input, CHARINDEX('|', input) + 1, LEN(input)) AS numbers
     FROM #raw_input
 )
 SELECT card
@@ -35,22 +35,45 @@ SELECT card
 INTO #owned_numbers
 FROM owned_numbers_list
 CROSS APPLY STRING_SPLIT(numbers, ' ')
-WHERE value <> ''
+WHERE value <> '';
 
-DROP TABLE IF EXISTS #winnings_per_card
+-- PART ONE
+
+DROP TABLE IF EXISTS #winnings_per_card;
 
 SELECT w.card
-    , 1 as card_amount
-    , COUNT(w.number) as winnings
+    ,ISNULL(j.winnings, 0) AS winnings
 INTO #winnings_per_card
-FROM #winning_number w
-INNER JOIN #owned_numbers o
-    ON w.card = o.card AND w.number = o.number
-GROUP BY w.card
+FROM (
+    SELECT DISTINCT card
+    FROM #winning_numbers
+) AS w
+LEFT JOIN (
+    SELECT w.card
+        , COUNT(w.number) AS winnings
+    FROM #winning_numbers w
+    INNER JOIN #owned_numbers o
+        ON w.card = o.card AND w.number = o.number
+    GROUP BY w.card
+) AS j ON w.card = j.card
 
-SELECT SUM(points)
+SELECT SUM(points) AS total_points
 FROM (
     SELECT card
-        ,POWER(2, winnings - 1) as points
+        ,POWER(2, winnings - 1) AS points
     FROM #winnings_per_card
-) t
+) t;
+
+-- PART TWO
+
+WITH rec AS (
+    SELECT *
+    FROM #winnings_per_card
+    UNION ALL
+    SELECT w.*
+    FROM rec as r
+    INNER JOIN #winnings_per_card AS w
+        ON w.card BETWEEN r.card + 1 AND r.card + r.winnings
+)
+SELECT COUNT(*) AS total_cards
+FROM rec

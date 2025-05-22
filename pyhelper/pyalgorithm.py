a6af3de2ce92a979647f091f2920fa56ebda3b5e
jdesven <89591153+jdesven@users.noreply.github.com>
2025-05-22 22:13:48 +0200

    add dijkstra to pyalgorithm. update type hints of breadth_first_search.

--- pyhelper/pyalgorithm.py
+++ pyhelper/pyalgorithm.py
@@ -1,4 +1,4 @@
-def breadth_first_search(map: set, pos_from: complex, pos_to: complex):
+def breadth_first_search(map: set[complex], pos_from: complex, pos_to: complex):
     paths = [[pos_from]]
     pos_seen = set([pos_from])
     while len(paths) > 0:
@@ -13,3 +13,20 @@ def breadth_first_search(map: set, pos_from: complex, pos_to: complex):
                     pos_seen.add(next_pos)
         paths = paths_next
     return []
+
+def dijkstra(pos_start: any, pos_end: any, connected_nodes: dict[any, set[tuple[any, int]]]) -> tuple[complex, int, list[list]]:
+    nodes_to_check = {pos_start: [0, [[]]]}
+    nodes_checked = set()
+    while len(nodes_to_check) > 0:
+        pos, (total_cost, routes) = min(nodes_to_check.items(), key = lambda x: x[1][0])
+        if (type(pos_end) in (list, tuple, set) and pos in pos_end) or pos == pos_end:
+            return pos, total_cost, [route + [pos] for route in routes]
+        for node, cost in connected_nodes[pos]:
+            if node not in nodes_checked:
+                if node not in nodes_to_check or nodes_to_check[node][0] > total_cost + cost:
+                    nodes_to_check[node] = [total_cost + cost, [route + [pos] for route in routes]]
+                elif nodes_to_check[node][0] == total_cost + cost:
+                    nodes_to_check[node][1].extend(route + [pos] for route in routes)
+        nodes_checked.add(pos)
+        nodes_to_check.pop(pos)
+    return tuple()

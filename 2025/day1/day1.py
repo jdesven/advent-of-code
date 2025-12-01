4f8f735fc8916764d5fb85b03f46c70e07fc821d
jdesven <89591153+jdesven@users.noreply.github.com>
2025-12-01 13:38:43 +0100

    2025 day 1

---
+++ 2025/day1/day1.py
@@ -0,0 +1,13 @@
+from pyhelper.pyimport import lines_to_list
+input = [50] + lines_to_list("2025\input\day1_input.txt")
+
+for i in range(1, len(input)):
+    input[i] = input[i - 1] + (int(input[i][1:]) * (1 if input[i][0] == 'R' else -1))
+
+print([num % 100 for num in input].count(0))
+
+print(sum((1 + abs(b - a) // 100) if b % 100 == 0
+    else (abs(b - a) // 100) if a % 100 == 0
+    else abs(b // 100 - a // 100)
+    for a, b in zip(input, input[1:])
+))

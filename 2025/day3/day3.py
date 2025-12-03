9692cca221fecdb4012461131befc4b8d27ad3db
jdesven <89591153+jdesven@users.noreply.github.com>
2025-12-03 21:44:11 +0100

    shorten the solution fo 2025 day 3

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -1,18 +1,15 @@
-banks = open("2025\input\day3_input.txt").read().split('\n')
+banks = open("2025\input\day3_input.txt").read().split()
     
-def calc(l):
-    ans = 0
-    for bank in banks:
-        num = ""
-        for i in range(l):
-            for n in map(str, range(9, -1, -1)):
-                if n in bank and len(bank) - bank.find(n) >= l - len(num):
-                    num += n
-                    bank = bank[bank.find(n)+1:]
-                    break
-        ans += int(num)
-    print(ans)
+def calc(l, bank):
+    num = ""
+    for i in range(l):
+        for n in map(str, range(9, -1, -1)):
+            if n in bank and len(bank) - bank.find(n) >= l - len(num):
+                num += n
+                bank = bank[bank.find(n)+1:]
+                break
+    return int(num)
 
-calc(2)
+print(sum(calc(2, bank) for bank in banks))
 
-calc(12)
+print(sum(calc(12, num) for num in banks))
